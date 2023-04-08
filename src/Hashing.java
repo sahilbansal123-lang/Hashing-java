556f06c0d44964cf36eb1e4bb828d735336e5a66
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-08 22:39:16 +0530

    hashing

---
+++ src/Hashing.java
@@ -0,0 +1,11 @@
+//    HashSet: is a collection in Java that stores unique elements. It implements the Set interface and uses a hash
+//             table for storage. HashSets do not maintain the order of the elements and allow null values.
+//
+//             In a HashSet, elements are stored based on their hash code value, which allows for constant-time
+//             performance for basic operations such as add, remove, and contains. HashSets also offer O(1) time
+//             complexity for these operations on average, although this can vary depending on the distribution of hash codes.
+public class Hashing {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
