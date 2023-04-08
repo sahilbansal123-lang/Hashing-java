3a55e78153ac98564c2ae640bcf8e9ddd6a57a2d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-08 22:59:31 +0530

    hashing

--- src/Hashing.java
+++ src/Hashing.java
@@ -4,8 +4,59 @@
 //             In a HashSet, elements are stored based on their hash code value, which allows for constant-time
 //             performance for basic operations such as add, remove, and contains. HashSets also offer O(1) time
 //             complexity for these operations on average, although this can vary depending on the distribution of hash codes.
+import java.util.HashSet;
+import java.util.Iterator;
+
+
 public class Hashing {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String args[]) {
+        HashSet<Integer> set = new HashSet<>();
+
+        //Add
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+
+        //Size
+        System.out.println("size of set is : " + set.size());
+
+
+        //Search
+        if(set.contains(1)) {
+            System.out.println("present");
+        }
+
+
+        if(!set.contains(6)) {
+            System.out.println("absent");
+        }
+
+
+        //Delete
+        set.remove(1);
+        if(!set.contains(1)) {
+            System.out.println("absent");
+        }
+
+
+        //Print all elements
+        System.out.println(set);
+
+
+        //Iteration - HashSet does not have an order
+        set.add(0);
+        Iterator it = set.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + ", ");
+        }
+        System.out.println();
+
+
+        //isEmpty
+        if(!set.isEmpty()) {
+            System.out.println("set is not empty");
+        }
     }
 }
