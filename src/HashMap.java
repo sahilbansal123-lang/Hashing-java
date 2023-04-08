e3710229166fa30ec705a020fc920de6d73911da
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-08 23:11:08 +0530

    hashing

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,3 +1,24 @@
+
+//        HashMap: is a collection that stores data in key-value pairs. It provides a way to store and retrieve objects
+//                 based on a unique key. The HashMap class is a part of the Java Collections Framework and is used to
+//                 implement the Map interface.
+//
+//        HashMap internally uses a hash table to store the key-value pairs. When an element is added to a HashMap,
+//        its key is used to calculate the hash code, which is then used as an index to store the key-value
+//        pair in the hash table.
+//
+//        The key is unique in the HashMap, and its hash code is used to locate the value associated with it.
+//        When you try to retrieve a value based on a key, the HashMap uses the same hash code to locate
+//        the value in the hash table. This makes the retrieval process very fast, even for large amounts of data.
+//
+//        Some key features of HashMap in Java are:
+//            It allows null keys and null values.
+//            It does not maintain the insertion order of the elements.
+//            It uses the hash code of the key to retrieve values, so it provides fast access to elements.
+//            It allows for duplicate values, but not duplicate keys.
+//        HashMaps are commonly used in Java for implementing caching mechanisms, for storing and retrieving
+//        data, and for implementing algorithms that require fast access to elements.
+
 public class HashMap {
     public static void main(String[] args) {
 
