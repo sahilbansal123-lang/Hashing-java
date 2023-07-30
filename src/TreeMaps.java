11fb65cec9f302ca7353280d12d1195ed07ec18b
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-30 21:37:25 +0530

    hashing

--- src/TreeMaps.java
+++ src/TreeMaps.java
@@ -7,7 +7,7 @@ public class TreeMaps {
         // Sorted on the basis of key
         tm.put("India", 120);
         tm.put("PAKISTAN", 100);
-        tm.put("ITALY", 110);
+        tm.put("Hungary", 110);
         tm.put("INDONESIA", 10);
         tm.put("CHINA", 90);
         System.out.println(tm);
