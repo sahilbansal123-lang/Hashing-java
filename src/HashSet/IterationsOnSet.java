b7f645e85f7c7415af3a0f64cec71c9137de8c07
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-01 23:00:11 +0530

    hashing

--- src/HashSet/IterationsOnSet.java
+++ src/HashSet/IterationsOnSet.java
@@ -1,4 +1,27 @@
 package HashSet;
+import java.util.*;
 
 public class IterationsOnSet {
+
+    public static void main(String[] args) {
+
+        HashSet<String> cities = new HashSet<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Pune");
+        cities.add("Kolkata");
+        cities.add("Hyderabad");
+
+
+        //  Iterator method Time Complexity O(N)
+        Iterator it = cities.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        //  Enhanced ForLoop Time Complexity O(N)
+
+
+
+    }
 }
