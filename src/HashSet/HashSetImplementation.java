925ac674a7f95cb645ca4fc396a49caf1c608f11
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-01 22:35:00 +0530

    hashing

--- src/HashSet/HashSetImplementation.java
+++ src/HashSet/HashSetImplementation.java
@@ -1,9 +1,28 @@
 package HashSet;
 
-public class HashSetImplementation {
+import java.util.HashSet;
 
+public class HashSetImplementation {
 
     public static void main(String[] args) {
 
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(2);
+        set.add(1);
+
+        System.out.println(set);
+        if (set.contains(2)) {
+            set.remove(2);
+            System.out.println(set);
+            System.out.println("element present");
+        }
+        if (!set.contains(6)) {
+            System.out.println("Element not present");
+        }
     }
 }
