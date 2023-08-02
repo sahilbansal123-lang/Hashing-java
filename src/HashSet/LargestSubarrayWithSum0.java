71d00bf128551d581c453bc025a57f9f85303797
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-02 12:06:24 +0530

    hashing

--- src/HashSet/LargestSubarrayWithSum0.java
+++ src/HashSet/LargestSubarrayWithSum0.java
@@ -1,9 +1,29 @@
 package HashSet;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class LargestSubarrayWithSum0 {
 
+    public static void largestSubArray(int []arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int length = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum)) {
+                length = Math.max(length, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        System.out.println("Length of subArray is: " + length);
+    }
 
     public static void main(String[] args) {
-
+        int arr[] = {15, 2, -2, -8, 1, 7, 10};
+        largestSubArray(arr);
     }
 }
