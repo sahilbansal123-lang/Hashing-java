bd79233ea4154216ebe2639b532810a8a3c1bd27
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-02 11:24:38 +0530

    hashing

--- src/HashSet/TicketsItenerary.java
+++ src/HashSet/TicketsItenerary.java
@@ -1,6 +1,23 @@
 package HashSet;
 
+import java.util.HashMap;
+
 public class TicketsItenerary {
+
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> revTic = new HashMap<>();
+
+        for (String key: tickets.keySet()) {
+            revTic.put(tickets.get(key), key);
+        }
+
+        for (String key: tickets.keySet()) {
+            if (!revTic.containsKey(key)) {
+                return key;
+            }
+        }
+        return null;
+    }
 
 
     public static void main(String[] args) {
